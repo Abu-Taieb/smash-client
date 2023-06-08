1eec61cd93c178b4d08db1edbc197a0e84d39df2
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-06-09 02:31:52 +0600

    Tournaments Section Design complete

--- src/Components/Pages/Home/Tournaments/Tournaments.jsx
+++ src/Components/Pages/Home/Tournaments/Tournaments.jsx
@@ -3,19 +3,17 @@ import { Link } from "react-router-dom";
 
 const Tournaments = () => {
     return (
-        <div className="w-full mx-auto justify-center pb-10 mb-10">
+        <div className="w-full mx-auto justify-center px-10 pb-10 mb-10">
         <div className="text-center md:w-7/12 mx-auto">
                 <h5 className="font-bold text-lg uppercase mb-2">Our Announcement</h5>
                 <hr className="w-20 mx-auto border-2 border-red-600" />
                 <h2 className="font-bold text-4xl uppercase my-6">Open YOUR FAVORITE Tournaments</h2>
                 <p className="text-slate-500">To gather the most recent and accurate information about the tournament, I recommend visiting reliable sports news websites.</p>
             </div>
-        <div className="flex mt-10 pt-5 px-10 gap-12 items-center">
-            <div className="w-1/3">
-                <img src="https://templatekit.jegtheme.com/smash/wp-content/uploads/sites/308/2022/07/young-woman-playing-badminton-7D3LTYJ-min-min-791x1024.png" alt="" />
-            </div>
-            <div className="w-2/3 bg-red-50 p-10">
-                <div className=""><h3 className="uppercase bg-white py-4 px-3 rounded-3xl font-bold w-96"><span className="bg-black text-white font-bold py-2 px-5  rounded-2xl mr-5">Tournaments</span> coming soon event</h3></div>
+        <div className="flex-row-reverse lg:flex mt-10 pt-5 px-0 lg:px-10 gap-12 items-center">
+            
+            <div className="w-full lg:w-2/3 bg-red-50 p-10">
+                <div className=""><h3 className="uppercase bg-white py-4 px-3 rounded-3xl font-bold w-96"><span className="bg-black text-white font-bold py-2 px-5 rounded-2xl mr-5">Tournaments</span> coming soon event</h3></div>
                 <h2 className="text-4xl uppercase font-bold py-9">Bangladesh <span className="text-red-600">BADMINTON</span>  OPEN 2023</h2>
                 <p className="text-slate-500">The Bangladesh Badminton Open is an annual badminton tournament held in Bangladesh. However, as of my knowledge cutoff in September 2021, there is no specific information available regarding the Bangladesh Badminton Open 2021.</p>
                 <div className="py-10">
@@ -29,6 +27,9 @@ const Tournaments = () => {
                     </div>
                 </div>
                 <Link to={'/logIn'} className="btn bg-gradient-to-r from-red-700 to-white text-white font-bold hover:from-white hover:to-red-700 border-0">Buy Ticket</Link>
+            </div>
+            <div className="w-full lg:w-1/3 py-10">
+                <img src="https://templatekit.jegtheme.com/smash/wp-content/uploads/sites/308/2022/07/young-woman-playing-badminton-7D3LTYJ-min-min-791x1024.png" alt="" />
             </div>
         </div>
         </div>
